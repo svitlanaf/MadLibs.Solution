12d4d38369034088f638696edaa1fd83cb149658
Svitlana Filatova <svitlana.filatova.gmail.com>
2019-04-29 20:15:59 -0700

    Add getters and setters

--- FriendLetter/Controllers/HomeController.cs
+++ FriendLetter/Controllers/HomeController.cs
@@ -1,13 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
+using FriendLetter.Models;
 
 namespace FriendLetter.Controllers
 {
   public class HomeController : Controller
   {
-
+    [Route("/hello")]
     public string Hello() { return "Hello friend!"; }
 
+    [Route("/goodbye")]
     public string Goodbye() { return "Goodbye friend."; }
 
+    [Route("/")]
+    public ActionResult Letter()
+      {
+        LetterVariable myLetterVariable = new LetterVariable();
+        myLetterVariable.SetRecipient("Lina");
+        myLetterVariable.SetSender("John");
+        return View(myLetterVariable);
+      }
+
+    [Route("/journal")]
+    public ActionResult Journal() {return View();}
   }
 }
